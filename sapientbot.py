0ecff8bfb1b05f0013b6d7d9b8c5e16aef1cb9e0
Johnna Koonce <159973348+annkoonce@users.noreply.github.com>
2024-03-01 16:49:14 -0500

    Update sapientbot.py

--- sapientbot.py
+++ sapientbot.py
@@ -1,51 +1,63 @@
-import os
+import logging
+import sys
+import threading
+
+from flask import Flask, request
 import discord
 from discord.ext import commands
-import spotipy
+import requests
+from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-import spacy
-from transformers import pipeline
+from chatterbot.conversation import Statement
+from flask import request
 import keepalive
 
-DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+# Set up logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.DEBUG)
 
-nlp = spacy.load('en_core_web_sm')
-sentiment_analysis = pipeline('sentiment-analysis')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+handler = logging.FileHandler(r'G:\SAPIENTBOT\SAPIENTBOT0.0.2.0\Logs\SBLogs.log')
+handler.setFormatter(formatter)
+
+log.addHandler(handler)
+
+app = Flask(__name__)
 
 chatbot = ChatBot('SAPIENTBOT')
 trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Train the chatbot based on the english corpus
+
+
 trainer.train("chatterbot.corpus.english")
 
-#BOT INTENTS & COOMMANDS !!
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_input = request.json["user_input"]
+    response = chatbot.get_response(user_input)
+    return {"response": str(response)}
+
+@app.route('/')
+def home():
+    app.logger.debug('This is a DEBUG message')
+    app.logger.info('This is an INFO message')
+    app.logger.warning('This is a WARNING message')
+    app.logger.error('This is an ERROR message')
+    app.logger.critical('This is a CRITICAL message')
+    return 'Hello SAPIENTBOT is Online & Ready.'
+
+# Create a new bot instance
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix=['/', '!'], intents=intents)
-async def check_role(ctx, *, role_name):
-    role = discord.utils.get(ctx.guild.roles, name=role_name)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-    if role in ctx.author.roles:
-        await ctx.send('You have this role.')
-    else:
-        await ctx.send('You do not have this role.')
-
-#Restricted commands & Cooldowns
-@bot.command()
-@commands.cooldown(1, 5, commands.BucketType.user)
-async def my_command(ctx):
-    await ctx.send('This command has a cooldown of 5 seconds per user.')
-@commands.has_role('Admin')
-async def restricted(ctx):
-    await ctx.send('You have access to this command.')
-
-#Console bot is online
 @bot.event
 async def on_ready():
     print('SAPIENTBOT is online & ready.')
 
-#NLP & Grammar
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -55,28 +67,41 @@ async def on_message(message):
         await message.channel.send('You mentioned me!')
         await message.channel.send('How can I help you today?')
     else:
-        doc = nlp(message.content)
-        entities = [ent.text for ent in doc.ents]
-        result = sentiment_analysis(message.content)[0]
-        response = f"I detected the following entities in your message: {', '.join(entities)}"
-        response += f"\nThe sentiment of your message is {result['label']} with a score of {result['score']}."
-        response += f"\nChatbot response: {chatbot.get_response(message.content)}"
+        response = chatbot.get_response(message.content)
         await message.channel.send(response)
 
     await bot.process_commands(message)
 
-#SPOTIFY CODE
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=(keepalive.SPOTIFY_CLIENT_ID),
-                                                           client_secret=(keepalive.SPOTIFY_CLIENT_SECRET)))
 @bot.command()
 async def play(ctx, *, song_name):
+    sp = Spotify(client_credentials_manager=SpotifyClientCredentials())
     results = sp.search(q=song_name, limit=1)
     if results['tracks']['items']:
-        song = results['tracks']['items'][0]
-        await ctx.send(f'Now playing: {song["name"]}\n{song["external_urls"]["spotify"]}')
+        await ctx.send(f'Now playing: {song_name}')
     else:
-        await ctx.send('Song not found')
-    
-#BOT RUN
+        await ctx.send('Song not found on Spotify.')
+
+@bot.command()
+async def recommend_streamers(ctx):
+    streamers = ['Streamer1', 'Streamer2', 'Streamer3']
+    await ctx.send(f'Check out these Twitch streamers: {", ".join(streamers)}')
+
+@bot.command()
+async def news(ctx):
+    news_api_key = 'your_news_api_key_here'
+    news_url = f'https://newsapi.org/v2/top-headlines?category=technology&apiKey={news_api_key}'
+    response = requests.get(news_url)
+    if response.status_code == 200:
+        articles = response.json().get('articles', [])
+        headlines = [article['title'] for article in articles]
+        await ctx.send('\n'.join(headlines[:5]))
+    else:
+        await ctx.send('Unable to fetch news at the moment.')
+
+#Tells Bot to log on with credentials
 def run_bot():
     bot.run(keepalive.DISCORD_BOT_TOKEN)
+
+if __name__ == "__main__":
+    run_bot()
+    app.run(host='0.0.0.0', port=8000)
